c3b5cdadc798c177b72e5b130257bbaf8002d6b0
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-04-07 14:13:14 -0500

    added html skeleton

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+//Screen loads with Title, instructions, and start button
+//When start button is clicked,
+    //button disappears
+    //render question and four answers
+    //render countdown timer for 30 seconds
+
+//Player clicks answer
+    //Check if answer is correct
+        //If answer is correct
+            //render: YES!, The correct answer was " " + gif
+        //If answer is incorrect
+            //render: No!, The correct answer was "" + gif
+//Show right/wrong answer with gif for set time (~5secs?)
+//Render next question
+//After all questions, display end game stats
+    //quesitons right
+    //questions wrong
+    //unanswered
+    //Start Over button
+//When start over button is clicked, 
+    //Return to startScreen
+
+$("start-btn").on("click", function(event){
+
+});
